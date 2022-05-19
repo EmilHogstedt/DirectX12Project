93c6c8c275d21312cc62953e928576707acd1798
Emil Fransson <emil.fransson.1@hotmail.com>
2022-05-19 09:35:08 +0200

    Local changes.

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -16,22 +16,22 @@ RaytracingAccelerationStructure scene : register(t0, space1);
 
 float4 main(in VS_OUT psIn) : SV_TARGET
 {
-    RayQuery<RAY_FLAG_CULL_NON_OPAQUE | RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES | RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH> query;
+    //RayQuery<RAY_FLAG_CULL_NON_OPAQUE | RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES | RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH> query;
 
-    RayDesc ray;
-    ray.Origin = psIn.outPositionSS.xyz;
-    ray.Direction = normalize((mul(float4(ray.Origin, 1.0f), vpInverseBuffer.VPInverseMatrix)).xyz);
-
-    ray.TMin = 0.0f;
-    ray.TMax = 1000.0f;
-
-    query.TraceRayInline(scene, 0, 0xFF, ray);
-    query.Proceed();
-
-    if (query.CommittedStatus() == COMMITTED_TRIANGLE_HIT)
-    {
-        return float4(1.0f, 1.0f, 1.0f, 1.0f);
-    }
+    //RayDesc ray;
+    //ray.Origin = psIn.outPositionSS.xyz;
+    //ray.Direction = normalize((mul(float4(ray.Origin, 1.0f), vpInverseBuffer.VPInverseMatrix)).xyz);
+    //
+    //ray.TMin = 0.0f;
+    //ray.TMax = 1000.0f;
+    //
+    //query.TraceRayInline(scene, 0, 0xFF, ray);
+    //query.Proceed();
+    //
+    //if (query.CommittedStatus() == COMMITTED_TRIANGLE_HIT)
+    //{
+    //    return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    //}
 
     return float4(psIn.outColor.xyz, 1.0f);
 }
